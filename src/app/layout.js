aa2925ccc5657b206b34ad8a2d8040cfe0804938
Therushmorare <tshego.tt.morare@gmail.com>
2025-10-24 02:13:01 +0200

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
-import "@/app/styles/globals.css"
-import Layout from "@/app/components/Layout.jsx"
+"use client"; // needed because we use hooks
+
+import "@/app/styles/globals.css";
+import Layout from "@/app/components/Layout.jsx";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  // Pages that should NOT have the Layout wrapper
+  const noLayoutPages = ["/", "/mfa", "/login"];
+
+  const useLayout = !noLayoutPages.includes(pathname);
+
   return (
     <html lang="en">
-      <body>
-        <Layout>
-          {children}
-        </Layout>
-      </body>
+      <body>{useLayout ? <Layout>{children}</Layout> : children}</body>
     </html>
   );
 }
-
